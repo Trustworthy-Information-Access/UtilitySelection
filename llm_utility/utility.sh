bf3346f0e233190265d5a043bb40db02596848d1
HengranZhang <54845883+hengran@users.noreply.github.com>
2026-03-18 17:00:46 +0800

    Update paths for model and dataset in utility.sh

--- llm_utility/utility.sh
+++ llm_utility/utility.sh
@@ -1,9 +1,9 @@
 
 
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
-model_name_or_path="/root/paddlejob/workspace/env_run/model/models/Qwen/Qwen3-32B"
-dataset_path="/root/paddlejob/workspace/env_run/output/sigir-ap/marco-train-100k.jsonl"
-output_dir="/root/paddlejob/workspace/env_run/output/sigir-ap/annotation_relevance_ranking.jsonl"
+model_name_or_path="models/Qwen/Qwen3-32B"
+dataset_path="sigir-ap/marco-train-100k.jsonl"
+output_dir="sigir-ap/annotation_relevance_ranking.jsonl"
 python llm-label-utility.py --model_name_or_path $model_name_or_path --dataset_path $dataset_path --batch_size 4096 --output_dir $output_dir --thinking False --type_prompt relevance_ranking
 
 dataset_path="sigir-ap/marco-train-100k.jsonl"
