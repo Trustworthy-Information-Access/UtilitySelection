32e4e4d5eee84cca6129db01053ccb0009baa00f
HengranZhang <54845883+hengran@users.noreply.github.com>
2026-03-18 17:00:22 +0800

    Update utility.sh

--- llm_utility/utility.sh
+++ llm_utility/utility.sh
@@ -1,13 +1,15 @@
 
 
-sleep 1m
 export VLLM_WORKER_MULTIPROC_METHOD=spawn
-model_name_or_path="model/models/Qwen/Qwen3-32B"
-dataset_path="datasets/marco-train-100k.jsonl"
-output_dir="results/utility_answer_both.jsonl"
-train_group_size=16
-python llm-label-utility.py --model_name_or_path $model_name_or_path --dataset_path $dataset_path --train_group_size $train_group_size --batch_size 4096 --output_dir $output_dir --thinking False --type_prompt utility
+model_name_or_path="/root/paddlejob/workspace/env_run/model/models/Qwen/Qwen3-32B"
+dataset_path="/root/paddlejob/workspace/env_run/output/sigir-ap/marco-train-100k.jsonl"
+output_dir="/root/paddlejob/workspace/env_run/output/sigir-ap/annotation_relevance_ranking.jsonl"
+python llm-label-utility.py --model_name_or_path $model_name_or_path --dataset_path $dataset_path --batch_size 4096 --output_dir $output_dir --thinking False --type_prompt relevance_ranking
 
+dataset_path="sigir-ap/marco-train-100k.jsonl"
+output_dir="sigir-ap/annotation_answer_nw1.jsonl"
+python llm-label-utility.py --model_name_or_path $model_name_or_path --dataset_path $dataset_path --batch_size 4096 --output_dir $output_dir --thinking False --type_prompt answer --nw_knowledge True
 
-
-
+dataset_path="sigir-ap/annotation_answer_nw1.jsonl"
+output_dir="sigir-ap/annotation_utility_nw1_def.jsonl"
+python llm-label-utility.py --model_name_or_path $model_name_or_path --dataset_path $dataset_path --batch_size 4096 --output_dir $output_dir --thinking False --type_prompt utility
